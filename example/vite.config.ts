4e8d0b0152b5a084fb0fc001fe26bb2fe7d50b18
Dmitry Litsman <d@litsman.com>
2024-03-24 18:13:09 +0000

    rename repo to match npm package

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.IS_GITHUB_PAGES ? "/react-wake-lock/" : "/",
+  base: process.env.IS_GITHUB_PAGES ? "/react-use-wake-lock/" : "/",
   plugins: [react()],
 });
